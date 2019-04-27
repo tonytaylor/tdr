d5053b76b7bd94d272325e7b12de2e1a9dfe6da0
Tony Taylor <tony@tonetaylor.com>
2019-04-27 01:16:53 -0500

    :wrench: initial implementation of CarouselButton

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -6,8 +6,29 @@ import CarouselButton from '../CarouselButton';
 configure({ adapter: new Adapter() });
 
 describe('CarouselButton', () => {
+  const txt = 'Button text';
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = wrapper = shallow(<CarouselButton children={txt} />);
+  });
+
   it('renders a <button>', () => {
-    const wrapper = shallow(<CarouselButton />);
     expect(wrapper.type()).toBe('button');
-  })
-})
+  });
+
+  it('passes `children` through to the <button>', () => {
+    expect(wrapper.prop('children')).toBe(txt);
+  });
+
+  it('passes other props through to the button', () => {
+    const onClick = () => {};
+    const className = 'my-carousel-button';
+    const dataAction = 'prev';
+    wrapper.setProps({ onClick, className, 'data-action': dataAction });
+
+    expect(wrapper.prop('onClick')).toBe(onClick);
+    expect(wrapper.prop('className')).toBe(className);
+    expect(wrapper.prop('data-action')).toBe(dataAction);
+  });
+});
