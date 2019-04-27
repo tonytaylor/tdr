52238878dbae8a72855dc6fdbc9a6f98d85a80e3
Tony Taylor <tony@tonetaylor.com>
2019-04-27 00:56:52 -0500

    added tests

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  env: { node: true },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline']
+  },
+  settings: {
+    react: { version: '16.4.2' }
+  }
+}
